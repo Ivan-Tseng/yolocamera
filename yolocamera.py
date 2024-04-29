a9b056b93c3bf844a155d7b2a92de8cfb8adafa1
Ivan-Tseng <49892761+Ivan-Tseng@users.noreply.github.com>
2024-04-29 15:28:28 +0800

    Add files via upload

---
+++ yolocamera.py
@@ -0,0 +1,35 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt') #輸入自己的訓練權重
+
+# Open the video file
+video_path = 0    #設置0代表用自己的攝影機
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
